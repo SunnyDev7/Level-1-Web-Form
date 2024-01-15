623051de5bcf217cea9c95bc0e0b7c6a79c01287
SunnyDev7 <sunnydev4589@gmail.com>
2024-01-15 22:27:46 +0530

    Responsiveness, color and local storage functionality added

--- logic.js
+++ logic.js
@@ -7,22 +7,28 @@ function submitForm() {
     var lastName = document.getElementById("lastname");
     var email = document.getElementById("email");
     var dob = document.getElementById("dob");
-    // var gender = document.querySelector('input[name="gender"]:checked');
+    var gender = document.getElementById("gender");
 
     // Validating each field
     if (firstName.value === "" || firstName.value.length < 3) {
         displayErrorMessage("firstnameError", "First Name is required and must be of min 3 characters");
         return; 
+    }else {
+        clearErrorMessage("firstnameError");
     }
     
-    if (lastName.value === "") {
+    if (lastName.value === "" || firstName.value.length < 3) {
         displayErrorMessage("lastnameError", "Last Name is required and must be of min 3 characters");
         return;
+    }else {
+        clearErrorMessage("lastnameError");
     }
 
     if (email.value === "") {
         displayErrorMessage("emailError", "Email Address is required");
         return;
+    }else {
+        clearErrorMessage("emailError");
     }
 
     // Validating email format
@@ -30,11 +36,15 @@ function submitForm() {
     if (!emailRegex.test(email.value)) {
         displayErrorMessage("emailError", "Please enter a valid email address");
         return;
+    } else {
+        clearErrorMessage("emailError");
     }
 
     if (dob.value === "") {
         displayErrorMessage("dobError", "Date of Birth is required");
         return;
+    } else {
+        clearErrorMessage("dobError");
     }
 
     // Validating date of birth
@@ -44,20 +54,49 @@ function submitForm() {
     if (selectedDate > currentDate) {
         displayErrorMessage("dobError", "Please enter a valid date of birth");
         return;
+    } else {
+        clearErrorMessage("dobError");
     }
 
-    // if (!gender) {
-    //     displayErrorMessage("genderError", "Gender is required");
-    //     return;
-    // }
+    if (gender.options[document.getElementById("gender").selectedIndex].textContent == " ") {
+        displayErrorMessage("genderError", "Gender is a required field");
+        return;
+    } else {
+        clearErrorMessage("genderError");
+    }
 
-    // If all validations pass, you can proceed with form submission or other actions
+    //If all the validation pass, store form values in local storage
+    var data = {
+        "firstName": firstName.value,
+        "lastName": lastName.value,
+        "email": email.value,
+        "dob": dob.value,
+        "gender": gender.value
+    };
+
+    console.log("Form Data:", data)
+
+    localStorage.setItem("data", JSON.stringify(data));
+
     alert("Form submitted successfully!");
+
+    resetFormValues();
+}
+
+function resetFormValues() {
+    // Resetting form values to empty state
+    var form = document.querySelector('form');
+    form.reset();
 }
 
 function displayErrorMessage(elementId, message) {
     var errorElement = document.getElementById(elementId);
     errorElement.textContent = message;
+}
+
+function clearErrorMessage(elementId) {
+    var errorElement = document.getElementById(elementId);
+    errorElement.textContent = ""; 
 }
 
 function resetErrorMessages() {
